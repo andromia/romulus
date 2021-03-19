dca21f1a8062664a5d2b380548a534276721a8af
chris <christophpryer@gmail.com>
2021-03-18 21:42:14 -0400

    updated assets used & added skipLibCheck to tsconfig

--- src/Logo.tsx
+++ src/Logo.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from 'react';
 import Image from 'react-bootstrap/Image';
 
 const Logo = (): ReactElement => {
-  return <Image src="../assets/icon.png" width="200px" alt="icon" />;
+  return <Image src="../assets/icon_white.png" width="200px" alt="icon" />;
 };
 
 export default Logo;
