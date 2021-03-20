0a489e4e0377e50acb1a1e8b75c7660db0eecdfb
chris <christophpryer@gmail.com>
2021-03-19 20:09:03 -0400

    started ClickableUploadIcon setFile

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,4 +1,6 @@
-import React, { ReactElement } from 'react';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { ReactElement, useState } from 'react';
 import Image from 'react-bootstrap/Image';
 
 type ImageProperties = {
@@ -8,22 +10,36 @@ type ImageProperties = {
 
 const ClickableUploadIcon = (props: ImageProperties): ReactElement => {
   const { height, width } = props;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [file, setFile] = useState({ preview: '', raw: '' });
+
+  const handleChange = (e: any) => {
+    if (e.target.files.length) {
+      setFile({
+        preview: URL.createObjectURL(e.target.files[0]),
+        raw: e.target.files[0],
+      });
+    }
+  };
 
   return (
     <div>
-      <Image
-        className="interactive-icon"
-        src="../assets/upload.svg"
-        width={width}
-        height={height}
-        alt="icon"
-        onClick={() => document.getElementById('clickable-upload')?.click()}
-      />
+      <label htmlFor="clickable-upload">
+        <Image
+          className="interactive-icon"
+          src="../assets/upload.svg"
+          width={width}
+          height={height}
+          alt="icon"
+        />
+      </label>
+      <p>{file.preview}</p>
       <input
         id="clickable-upload"
         className="file-upload"
         type="file"
         name="upload"
+        onChange={handleChange}
       />
     </div>
   );
