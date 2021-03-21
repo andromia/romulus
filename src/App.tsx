37aad63f5b98bb669ab9fc93d79ce7d7f35c0ecc
chris <christophpryer@gmail.com>
2021-03-20 23:41:58 -0400

    started fleshing out FileProfile UI

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import {
 } from 'react-router-dom';
 import './App.global.css';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Table from 'react-bootstrap/Table';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
@@ -92,6 +94,67 @@ const FileProfile = () => {
             </Button>
           </Col>
         </Row>
+      </Container>
+      <Container>
+        <h1>File Name</h1>
+        <Button className="script-run-button">Run</Button>
+        <Button className="script-remove-button">Remove</Button>
+        <hr color="#45484e" />
+        <Container className="file-profile-form-container">
+          <Form>
+            <Form.Group>
+              <Form.Label className="file-profile-form-label">Date</Form.Label>
+              <Form.Control type="date" />
+              <Form.Label className="file-profile-form-label">Time</Form.Label>
+              <Form.Control type="time" />
+            </Form.Group>
+          </Form>
+        </Container>
+        <Container className="file-profile-history-table-container">
+          {/* TODO: use d3.js or query framework */}
+          <Table
+            striped
+            bordered
+            hover
+            variant="dark"
+            className="file-profile-history-table"
+          >
+            <thead>
+              <tr>
+                <th>Start</th>
+                <th>Finish</th>
+                <th>Duration</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Container>
       </Container>
     </div>
   );
