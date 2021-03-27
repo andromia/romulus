3453c505766d3bf65115a9899620fae3dbc1add6
chris <christophpryer@gmail.com>
2021-03-27 15:41:01 -0400

    updated UploadIcon w/ dialog filtered files (closes #9)

--- src/components/UploadIcon.tsx
+++ src/components/UploadIcon.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useRef } from 'react';
+import React from 'react';
 import Image from 'react-bootstrap/Image';
 import icon from '../../assets/upload.svg';
 import {
@@ -9,19 +9,16 @@ import {
   sortStore,
 } from '../common';
 
+const { dialog } = require('electron').remote;
+
 const Store = require('electron-store');
 
 const store = new Store();
 
 const ClickableUploadIcon = (props: ClickableUploadIconType) => {
   const { setFiles, height, width } = props;
-  const inputRef = useRef(null);
 
-  const handleChange = (e: any) => {
-    if (!e.target.files.length) {
-      return;
-    }
-
+  const handleUploadClick = () => {
     // create unique id for uploaded files
     let k = 0;
     const ids = Object.keys(store.store).map((key) => store.store[key].id);
@@ -30,50 +27,46 @@ const ClickableUploadIcon = (props: ClickableUploadIconType) => {
       k = Math.max(...ids) + 1;
     }
 
-    for (let i = 0; i < e.target.files.length; i += 1) {
-      if (!e.target.files[i].path.endsWith('.py')) {
-        return; // TODO: notification?
-      }
+    dialog
+      .showOpenDialog({
+        properties: ['openFile', 'multiSelections'],
+        filters: [{ name: 'Python Files', extensions: ['py'] }],
+      })
+      .then((result: { canceled: any; filePaths: any }) => {
+        // eslint-disable-next-line promise/always-return
+        for (let i = 0; i < result.filePaths.length; i += 1) {
+          const path = result.filePaths[i];
 
-      const { name, path } = e.target.files[i];
-      const key = parseKeyFromFilePath(path);
-      store.set(key, {
-        id: k,
-        name,
-        path,
-        pythonExe: 'python',
+          // eslint-disable-next-line no-useless-escape
+          const name = path.replace(/^.*[\\\/]/, '');
+          const key = parseKeyFromFilePath(path);
+          store.set(key, {
+            id: k,
+            name,
+            path,
+            pythonExe: 'python',
+          });
+
+          k += 1;
+        }
+
+        const sortedStore = sortStore(store.store);
+        setFiles(sortedStore);
+      })
+      .catch((err: any) => {
+        console.log(err);
       });
-
-      k += 1;
-    }
-
-    const sortedStore = sortStore(store.store);
-    setFiles(sortedStore);
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore: Object is possibly 'null'.
-    inputRef.current.value = '';
   };
 
   return (
     <div className="upload-icon-div">
-      <label htmlFor="clickable-upload">
-        <Image
-          className="interactive-icon upload-icon"
-          src={icon}
-          width={width}
-          height={height}
-          alt="icon"
-        />
-      </label>
-      <input
-        id="clickable-upload"
-        className="file-upload"
-        type="file"
-        name="upload"
-        onChange={handleChange}
-        ref={inputRef}
-        multiple
+      <Image
+        className="interactive-icon upload-icon"
+        src={icon}
+        width={width}
+        height={height}
+        alt="icon"
+        onClick={handleUploadClick}
       />
     </div>
   );
